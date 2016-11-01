62990e0eeee45b04fc467fad687e8ef4bcb248d5
endo <takashi.endo@cluez.jp>
2016-11-01 12:49:03 +0900

    とりあえず、Gmailから認証コードを引っ張り出すのに成功

--- main.js
+++
@@ -1,3 +0,0 @@
-import loginTwitter from './webdriver';
-
-loginTwitter()
--- main.py
+++ main.py
@@ -1,78 +1,38 @@
+# -*- coding: utf-8 -*-
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+import re
 driver = webdriver.Chrome()
+GMAIL_ADRESS = "frabro568@gmail.com"
+GMAIL_PASS = "ndagmabry9"
 
 driver.get("https://accounts.google.com/ServiceLogin?service=mail&continue=https://mail.google.com/mail/&hl=ja")
 
 #page1
 elem = driver.find_element_by_css_selector('#Email')
-elem.send_keys("godfri30@gmail.com")
+elem.send_keys(GMAIL_ADRESS)
 elem = driver.find_element_by_css_selector('#next').click()
 sleep(0.5)
 elem = driver.find_element_by_css_selector('#Passwd')
-elem.send_keys("idsnave9da")
+elem.send_keys(GMAIL_PASS)
 sleep(0.5)
 elem = driver.find_element_by_css_selector('#signIn').click()
-
+sleep(5)
+elems = driver.find_elements_by_css_selector('span')
+for e in elems:
+    if e.text == '40404':
+        e.click()
+        break
+elems = driver.find_elements_by_css_selector('div')
+sleep(3)
+for e in elems:
+    # print(e.text)
+    pattern = r"Twitter認証コードは([0-9]*)です"
+    repatter = re.compile(pattern)
+    matchOB = repatter.findall(e.text)
+    if len(matchOB) > 0:
+        print("コード is:",matchOB[0])
+        break
 # driver.switch_to_window(driver.window_handles[-1])
-
-# page2
-elem = driver.find_element_by_css_selector('#top > div.bg-block > div > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > span > input[type="text"]')
-elem.send_keys("62837393")
-sleep(0.5)
-
-elem = driver.find_element_by_css_selector('#top > div.bg-block > div > div > table > tbody > tr:nth-child(2) > td:nth-child(3) > span > input[type="password"]')
-elem.send_keys("1920")
-sleep(0.5)
-
-elem = driver.find_element_by_css_selector('#top > div.bg-block > div > div > table > tbody > tr:nth-child(3) > td:nth-child(3) > span > input[type="text"]')
-elem.send_keys("3006")
-sleep(0.5)
-
-elem = driver.find_element_by_css_selector('#top > div.bg-block > div > div > table > tbody > tr:nth-child(3) > td:nth-child(4) > p > a')
-elem.click()
-
-# page3
-elem = driver.find_element_by_css_selector('#top > div.bg-block > div > form > p > input[type="submit"]')
-elem.click()
-
-# page4
-elem = driver.find_element_by_css_selector('#b_area > div:nth-child(3) > ul > li:nth-child(1) > a')
-elem.click()
-
-# page5
-elem = driver.find_element_by_css_selector('#gi_area > div > table > tfoot > tr > td > table > tbody > tr:nth-child(2) > td.auto > p.button > input[type="submit"]')
-elem.click()
-
-# page6
-elem = driver.find_element_by_css_selector('#vi_area > div > div.appli > p:nth-child(1) > input[type="text"]')
-elem.send_keys("12")
-sleep(0.5)
-
-elem = driver.find_element_by_css_selector('#vi_area > div > div.appli > p:nth-child(2) > input[type="text"]')
-elem.send_keys("10")
-sleep(0.5)
-
-elem = driver.find_element_by_css_selector('#vi_area > div > div.editbtn > span > input[type="button"]')
-elem.click()
-
-# page6
-elem = driver.find_element_by_css_selector('#i_area > table > tbody > tr:nth-child(1) > td > input[type="text"]')
-elem.clear()
-elem.send_keys("62837393")
-sleep(0.5)
-
-elem = driver.find_element_by_css_selector('#i_area > table > tbody > tr:nth-child(2) > td > input[type="password"]')
-elem.send_keys("1920")
-sleep(0.5)
-
-elem = driver.find_element_by_css_selector('#i_area > table > tbody > tr:nth-child(3) > td > input[type="text"]')
-elem.clear()
-elem.send_keys("3006")
-sleep(0.5)
-
-elem = driver.find_element_by_css_selector('#i_area > table > tbody > tr:nth-child(4) > td > input[type="text"]:nth-child(1)')
-elem.send_keys("12000")
-sleep(0.5)
-# driver.close()
